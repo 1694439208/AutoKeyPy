f5d3d43cec38f8c0422df7cb9112109157d8b8b3
1694439208 <1694439208@qq.com>
2025-01-14 19:09:04 +0800

    Update 简单压枪.py

--- 简单压枪.py
+++ 简单压枪.py
@@ -12,7 +12,7 @@ def main(arg):
         print(f'按下-参数:{arg}')
         hmbb.down(120,arg[0],arg[1])
         while ya:
-          hmbb.motton(0,1) #每次压枪 x不动，y压1像素
+          hmbb.motton(hmbb.get_mouseId(),0,1) #每次压枪 x不动，y压1像素
           time.sleep(50/1000) #压枪间隔50ms
           print(f'压枪00中.')
         print(f'压枪完毕.')
